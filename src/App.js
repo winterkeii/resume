6d3abdf5e62f02e12dfb18099e2ef0ed21a4af40
winterkeii <johnleomedina@gmail.com>
2024-09-19 17:12:22 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngular, faCss3, faGithub, faGoogle, faHtml5, faLinkedin, faPython, faReact, faVuejs } from "@fortawesome/free-brands-svg-icons";
+import {} from "@fortawesome/free-regular-svg-icons"
+import { faLocationPin, faPhone} from "@fortawesome/free-solid-svg-icons"
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="Resume">
+      <div className="Header">
+        <h1>John Leo Medina</h1>
+        <h2><em>DIRECTOR OF SOFTWARE ENGINEERING</em></h2>
+      </div>
+      <div className="Content">
+        <div className="Contact">
+        <h1><strong>CONTACT</strong></h1>
+       
+          <ul>jlmedina.student@ua.edu.ph<FontAwesomeIcon icon={faGoogle} pull = "right" /></ul>
+          <ul>09055088481 <FontAwesomeIcon icon={faPhone} pull = "right"/></ul>
+          <ul>Arayat, Pampanga <FontAwesomeIcon icon={faLocationPin} pull = "right"/> </ul>
+          <ul><a href = "https://www.linkedin.com/login" target="_blank">linkedin <FontAwesomeIcon icon={faLinkedin} pull = "right"/></a></ul>
+          <ul><a href = "https://github.com/winterkeii" target="_blank">Github <FontAwesomeIcon icon={faGithub} pull = "right"/> </a></ul>
+        
+          <h1><strong>EDUCATION</strong></h1>
+       
+          <ul>Bachelor of Science</ul>
+          <ul>Information Technology</ul>
+          <ul>University of the</ul>
+          <ul>Assumption</ul>
+          <ul>2023-2025</ul>
+          <ul>San Fernando City, Pampanga</ul>
+
+          <h1><strong>SKILLS</strong></h1>
+       
+          <ul>React JS <FontAwesomeIcon icon={faReact} pull = "right" /></ul>
+          <ul>React Native <FontAwesomeIcon icon={faReact} pull = "right" /> </ul>
+          <ul>Vue JS <FontAwesomeIcon icon={faVuejs} pull = "right" /></ul>
+          <ul>Angular <FontAwesomeIcon icon={faAngular} pull = "right" /></ul>
+          <ul>Python <FontAwesomeIcon icon={faPython} pull = "right" /></ul>
+          <ul>Html & CSS <FontAwesomeIcon icon={faHtml5} pull = "right" /><FontAwesomeIcon icon={faCss3} pull = "right" /></ul>
+        </div>
+          
+        <div className="Experience">
+        <h1>WORK EXPERIENCE</h1>
+        <h3>Software Engineering</h3>
+        <h5>Google</h5>
+        <p>January 2016 - current / New York, NY
+          <p>Worked with product managers to re-architect a multi-page web app into a single page web-app, boosting yearly revenue by $1.4M</p>
+          <p>Constructed the logic for a streamlined ad-serving platform that scaled to our 35M users, which improved the page speed by 15%</p>
+        <p>Tested software for bugs and operating speed, fixing bugs and documenting processes to increase efficiency by 18%</p>
+        <p>Iterated platform for college admissions, collaborating with a group of 4 engineers to create features across the software</p>
+        </p><br></br>
+        <h3>Software Engineering</h3>
+        <h5>Paypal</h5>
+        <p>July 2011 - December 2015 / San Jose, CA
+          <p>Worked with product managers to re-architect a multi-page web app into a single page web-app, boosting yearly revenue by $1.4M</p>
+          <p>Constructed the logic for a streamlined ad-serving platform that scaled to our 35M users, which improved the page speed by 15%</p>
+        <p>Tested software for bugs and operating speed, fixing bugs and documenting processes to increase efficiency by 18%</p>
+        <p>Iterated platform for college admissions, collaborating with a group of 4 engineers to create features across the software</p>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <br></br>
+        <h3>Software Engineering</h3>
+        <h5>AMD</h5>
+        <p>October 2007 - May 2011 / Edmonton, Canada
+          <p>Worked with product managers to re-architect a multi-page web app into a single page web-app, boosting yearly revenue by $1.4M</p>
+          <p>Constructed the logic for a streamlined ad-serving platform that scaled to our 35M users, which improved the page speed by 15%</p>
+        <p>Tested software for bugs and operating speed, fixing bugs and documenting processes to increase efficiency by 18%</p>
+        <p>Iterated platform for college admissions, collaborating with a group of 4 engineers to create features across the software</p>
+        </p>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
-
-export default App;
